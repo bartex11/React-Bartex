802ac2cec65e1b1103e583ba55192f84d5812881
DOM\IvanVasilev <i.vasilev@spicymedia.io>
2018-12-06 10:56:07 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,77 +1,60 @@
-// @flow
-
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home';
+import Home1 from './components/Home1';
 
-function BasicExample() {
+const nav = [
+  {
+    name: 'Home',
+    slug: '/home',
+  },
+ {
+    name: 'Home1',
+    slug: '/home1',
+  },
+ 
+];
+
+const routes = [
+  {
+    path: "/home",
+    component: Home,
+  },
+  {
+    path: "/home1",
+    component: Home1,
+  }
+ 
+];
+
+
+function RouteConfigExample() {
   return (
     <Router>
       <div>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
+          {nav.map((item, index) => (
+          <li key={index}>
+            <Link 
+              to={item.slug}
+              key={item.slug}
+              >{item.name}
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
+          ))}
         </ul>
-
-        <hr />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+        
+        {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
       </div>
     </Router>
   );
 }
 
-
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
-
-export default BasicExample;
+export default RouteConfigExample;
---
+++ src/components/Home1.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Home() {
+    return (
+        <header className="navbar">This is the BBBBBBBBBBBBBBBBBBBBbbbbbbbbb</header>
+    )
+}
+
+export default Home
